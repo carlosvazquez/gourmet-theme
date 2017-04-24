007c28f0b94610f71b5e3a350b5bce41231b8bcb
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2017-04-23 22:22:40 -0500

    Add slider, customer pages, google fonts, fix script

--- findreplace.js
+++ findreplace.js
@@ -14,3 +14,10 @@ replace({
     recursive: true,
     silent: true,
   });
+replace({
+    regex: "sans-serif\"",
+    replacement: "sans-serif",
+    paths: ['./dist/assets/styles.css.twig'],
+    recursive: true,
+    silent: true,
+  });
